ee5b0f5c784bb16f1328c3276c28a8e8baf0cbdd
Niels Bantilan <niels.bantilan@gmail.com>
2022-01-18 15:05:27 -0500

    update setup.py description
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="flytekit-learn",
     version="0.0.0+dev0",
-    description="The easiest way to build and deploy models.",
+    description="The easiest way to build and deploy machine learning services.",
     author="unionai-oss",
     author_email="info@union.ai",
     packages=find_packages(exclude=["tests*"]),
