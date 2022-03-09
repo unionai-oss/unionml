296ea7ed19eccde3862f76ecfef746d822ccea07
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2022-03-09 00:34:41 -0800

    Add aws-lambda-example
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

---
+++ aws-lambda-example/ulearn_example/__init__.py
@@ -0,0 +1,9 @@
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+# TODO: remove! this is just for demo
+import logging
+
+from flytekit_learn.dataset import Dataset
+from flytekit_learn.model import Model
+
+logging.basicConfig(level=logging.ERROR)
