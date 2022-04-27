c1b4e6c2b86029b9a0f3aa0d760fde4b61dc5cde
Yuvraj <evalsocket@users.noreply.github.com>
2022-04-27 23:45:19 +0530

    feat: added ci for publishing pypi package
    Signed-off-by: Yuvraj <evalsocket@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 
+__version__ = "0.0.0+develop"
+
 setup(
     name="unionml",
-    version="0.0.0+dev0",
+    version=__version__,
     description="The easiest way to build and deploy machine learning services.",
     author="unionai-oss",
     author_email="info@union.ai",
