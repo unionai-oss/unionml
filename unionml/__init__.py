1f7f10a3943e09899b67bb02215cc5019117cb58
Sugato Ray <sugatoray@users.noreply.github.com>
2022-06-23 19:23:34 -0500

    Update source packaging (#125)
    * update MANIFEST.in
    
    * add .gitattributes
    
    * update setup.py
    - add urls
    - parameterize LICENSE string for single point control
    - minor other modifications
    
    * single source version from setup.py
    - changes unionml/__init__.py
    - changes workflow: publish.yml
    
    * remove author annotations.
    
    * update MANIFEST.in
    
    * updates and corrections:
    - setup.py: use README for log_description
    - publish.yml: prune steps no longer needed
    
    * pruned workflow
    
    * update setup.py
    
    * add importlib-metadata to requirements
    
    * Update .gitattributes
    
    * Update __init__.py
    
    * pre-commit infused updates
    
    * added comment for single sourcing version
    
    * install importlib-metadata only for py<38
    
    * Update __init__.py
    
    Co-authored-by: Niels Bantilan <niels.bantilan@gmail.com>

--- unionml/__init__.py
+++ unionml/__init__.py
@@ -4,6 +4,16 @@ import re
 from unionml.dataset import Dataset
 from unionml.model import Model
 
+# necessary for single sourcing version
+try:
+    from importlib import metadata  # type: ignore
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata  # type: ignore
+
+# single source version from setup.py
+__title__ = __name__
+__version__ = metadata.version(__title__)  # type: ignore
+
 
 # silence FlyteRemote beta warning since unionml won't expose it to end users.
 class FlyteRemoteFilter(logging.Filter):
@@ -23,6 +33,3 @@ flytekit_logger.addFilter(PickleFilter())
 
 flytekit_remote_logger = flytekit_logger.getChild("remote")
 flytekit_remote_logger.addFilter(FlyteRemoteFilter())
-
-
-__version__ = "0.0.0+dev0"
